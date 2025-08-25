0c67f7e943599d77660e2dc29be0d7a7c7e2f168
silvioiatech <silvio.ia.tech@gmail.com>
2025-08-26 00:17:50 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,7 @@
+# bot/core/__init__.py
+from .config import SystemConfig
+from .database import DatabaseManager
+from .logger import logger, setup_logging
+from .security import require_auth
 
+__all__ = ['SystemConfig', 'DatabaseManager', 'logger', 'setup_logging', 'require_auth']
