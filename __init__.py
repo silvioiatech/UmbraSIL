60d779941dd37759e5bb4f4da8266a152db79c6f
silvioiatech <silvio.ia.tech@gmail.com>
2025-08-26 02:31:54 +0200

    Refactor __init__.py for UmbraSIL Bot Package

--- __init__.py
+++ __init__.py
@@ -1,11 +1,40 @@
 """
-Personal Bot Assistant
-Version: 1.0.0
-Author: silvioiatech
+UmbraSIL Bot Package
+Created by: silvioiatech
+Created at: 2025-08-26 00:31:13
 """
 
-from .core import *
-from .modules import *
+from .core import (
+    DatabaseManager,
+    SecurityManager,
+    BotError,
+    DatabaseError,
+    AuthenticationError,
+    require_auth,
+    logger
+)
+
+from .modules.finance import FinanceManager
+from .modules.business import BusinessManager
+from .modules.monitoring import MonitoringManager
+from .modules.ai import AIManager, AIConfig
 
 __version__ = "1.0.0"
 __author__ = "silvioiatech"
+__created_at__ = "2025-08-26 00:31:13"
+
+# Make common classes available at package level
+__all__ = [
+    'DatabaseManager',
+    'SecurityManager',
+    'FinanceManager',
+    'BusinessManager',
+    'MonitoringManager',
+    'AIManager',
+    'AIConfig',
+    'BotError',
+    'DatabaseError',
+    'AuthenticationError',
+    'require_auth',
+    'logger'
+]
